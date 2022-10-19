0f1d364d47689e95845b0fb3b8f478cfa3cedfa8
lucvil <60900509+lucvil@users.noreply.github.com>
2022-10-19 16:45:40 +0900

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -1,6 +1,5 @@
  Shopify.Checkout.OrderStatus.addContentBox(
    'タイトル',
-   {%- comment -%} cart.attributesがあれば {%- endcomment -%}
    {% if attributes %}
      {% if attributes['配送日の指定'] == "指定する" %}
        `<p>希望配送日時は</p>
@@ -8,8 +7,6 @@
        `,
      {% endif %}
    {% endif %}
-
-   {%- comment -%} 入会への案内 {%- endcomment -%}
    `
    <b>入会特典！1000円クーポンプレゼント中！</b><br>
    <a href="/account/register">会員登録はこちら</a>
