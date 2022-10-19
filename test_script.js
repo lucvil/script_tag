eb936135024b909e2e747b05c563f9713064df98
lucvil <60900509+lucvil@users.noreply.github.com>
2022-10-19 16:34:00 +0900

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -1,4 +1,17 @@
-Shopify.Checkout.OrderStatus.addContentBox(
- '<h2>Pick-up in store</h2>',
- '<p>We are open everyday from 9am to 5pm.</p>'
-)
+ Shopify.Checkout.OrderStatus.addContentBox(
+   'タイトル',
+   {%- comment -%} cart.attributesがあれば {%- endcomment -%}
+   {% if attributes %}
+     {% if attributes['配送日の指定'] == "指定する" %}
+       `<p>希望配送日時は</p>
+        <p>{{ attributes['配送希望日'] | date: "%Y年%m月%d日" }}の{{ attributes['配送時間帯'] }}です。</p>
+       `,
+     {% endif %}
+   {% endif %}
+
+   {%- comment -%} 入会への案内 {%- endcomment -%}
+   `
+   <b>入会特典！1000円クーポンプレゼント中！</b><br>
+   <a href="/account/register">会員登録はこちら</a>
+   `
+ )
