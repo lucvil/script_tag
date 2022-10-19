9d923bb6a8e7fc5b1daaf2e304ae14219da87a9f
lucvil <60900509+lucvil@users.noreply.github.com>
2022-10-19 16:17:31 +0900

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -20,8 +20,22 @@
 //     }
 // }
 // console.log(customer.email);
-Shopify.Checkout.OrderStatus.addContentBox(
-   '<h2>限定クーポン情報</h2>',
-   '<p>次回ご利用いただけるクーポンをご用意しました。 クーポンコード「REWIRE2021」</p>',
-  '<a href="#your-store-name.html"><img src="your-store-coupon.jpg" / ></a>'
-)
+<script>
+  Shopify.Checkout.OrderStatus.addContentBox(
+    'タイトル',
+    {%- comment -%} cart.attributesがあれば {%- endcomment -%}
+    {% if attributes %}
+      {% if attributes['配送日の指定'] == "指定する" %}
+        `<p>希望配送日時は</p>
+         <p>{{ attributes['配送希望日'] | date: "%Y年%m月%d日" }}の{{ attributes['配送時間帯'] }}です。</p>
+        `,
+      {% endif %}
+    {% endif %}
+
+    {%- comment -%} 入会への案内 {%- endcomment -%}
+    `
+    <b>入会特典！1000円クーポンプレゼント中！</b><br>
+    <a href="/account/register">会員登録はこちら</a>
+    `
+  )
+</script>
