073d4a968ae54dc54713a6e9bd678b4b863c1cbd
lucvil <60900509+lucvil@users.noreply.github.com>
2022-10-19 17:08:39 +0900

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -1,10 +1,40 @@
- Shopify.Checkout.OrderStatus.addContentBox(
-   'タイトル',
-   `<p>希望配送日時は</p>
-    <p>{{ attributes['配送希望日'] | date: "%Y年%m月%d日" }}の{{ attributes['配送時間帯'] }}です。</p>
-   `,
-   `
-   <b>入会特典！1000円クーポンプレゼント中！</b><br>
-   <a href="/account/register">会員登録はこちら</a>
-   `
- )
+ try {
+var items = [];
+{% for line_item in cart.items %}
+items.push({
+  item_id: '{{line_item.sku}}',
+  name: '{{line_item.title}}',
+  price: '{{line_item.final_price}}' ? Number('{{line_item.final_price}}')/100 : null,
+  quantity: '{{line_item.quantity}}' ? Number('{{line_item.quantity}}') : null,
+  item_image_url: '{{line_item.image}}',
+  item_url: '{{line_item.url}}',
+  item_brand: '{{line_item.vendor}}',
+});
+{% endfor %}
+var item_ids = items.map( function(item) { return item.item_id; });
+var item_names = items.map( function(item) { return item.name; });
+var item_prices = items.map( function(item) { return item.price; });
+var item_quantities = items.map( function(item) { return item.quantity; });
+var item_image_urls = items.map( function(item) { return item.item_image_url; });
+var item_urls = items.map( function(item) { return item.item_url; });
+var item_brands = items.map( function(item) { return item.item_brand; });
+tracker.track('cart', {
+  price: '{{cart.items_subtotal_price}}' ? Number('{{cart.items_subtotal_price}}')/100 : null,
+  quantity: '{{cart.item_count}}' ? Number('{{cart.item_count}}') : null,
+  status: '{{cart.item_count}}' ? Number('{{cart.item_count}}')>0 : null,
+  item_ids: item_ids,
+  item_names: item_names,
+  item_prices: item_prices,
+  item_quantities: item_quantities,
+  item_image_urls: item_image_urls,
+  item_urls: item_urls,
+  item_brands: item_brands,
+  items: items,
+});
+} catch(e) {
+if ('tracker' in window) {
+  tracker.track('_error', {
+      message: e.message
+  });
+}
+}
