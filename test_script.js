c770077fccbe2f04be6009713a999e90364c68f4
lucvil <60900509+lucvil@users.noreply.github.com>
2022-10-20 19:48:43 +0900

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -1,6 +1,4 @@
-{% if checkout.shipping_address.country_code == 'JP'%}
-    Shopify.Checkout.OrderStatus.addContentBox(
-    '<h2>YOUR TITLE HERE</h2>',
+Shopify.Checkout.OrderStatus.addContentBox(
+    '<h2>YOUR TITLE HERE{{checkout.shipping_address.country_code }}</h2>',
     '<p>YOUR MESSAGE HERE</p>'
-    )
-{% endif %}
+)
