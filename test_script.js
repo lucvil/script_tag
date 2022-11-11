a62509c337de2552c98254d8e565c7a0bc55d6ce
lucvil <60900509+lucvil@users.noreply.github.com>
2022-11-11 14:59:43 +0900

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -2,3 +2,6 @@ Shopify.Checkout.OrderStatus.addContentBox(
     '<h2>YOUR TITLE HERE{{checkout.shipping_address.country_code }}</h2>',
     '<p>YOUR MESSAGE HERE</p>'
 )
+
+let order_id = window.Shopify.order.id
+console.log(order_id)
