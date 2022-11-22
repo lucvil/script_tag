0684ff7aec01764b280bbc9a76d1939b5f8ebe05
lucvil <60900509+lucvil@users.noreply.github.com>
2022-11-22 18:12:22 +0900

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -1,5 +1,5 @@
 Shopify.Checkout.OrderStatus.addContentBox(
-    '<h2>YOUR OERDER ID</h2>',
-    '<p>'+window.Shopify.checkout.order_id+'</p>'
+    '<h2>YOUR TITLE HERE</h2>',
+    '<p>YOUR MESSAGE HERE'+window.Shopify.checkout.order_id+'</p>',
+    '<form action="https://luckyvillages-sample.myshopify.com/apps/address_kun/test" method="GET"><input type="mytext" value="text"><input type="submit" value="送信"></form>'
 )
-
