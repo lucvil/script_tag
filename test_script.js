7ddb85dbfef2db5260a1a98e0fba521419942876
lucvil <60900509+lucvil@users.noreply.github.com>
2022-12-07 22:01:13 +0900

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -1,6 +1,6 @@
 Shopify.Checkout.OrderStatus.addContentBox(
 	'<dialog id="inputDialog"><form id="inputForm"><input name="city" placeholder="市区町村" required="" type="text" aria-required="true" autocomplete="shipping address-level2"><input name="address1" placeholder="住所" required="" type="text" aria-required="true" autocomplete="shipping address-line1"><input name="address2" placeholder="建物名、部屋番号など (任意)" type="text" aria-required="false" autocomplete="shipping address-line2"><button id="toConfirmButton" type="button">確認</button></form></dialog>',
-	'<dialog id="confirmDialog"><form id="confirmForm" action="https://luckyvillages-sample.myshopify.com/apps/address/test" method="get"><button id="submitButton" type="button">送信</button></form></dialog>'
+	'<dialog id="confirmDialog"><form id="confirmForm" action="https://luckyvillages-sample.myshopify.com/apps/address/test" method="get"><button id="submitButton" type="submit">送信</button></form></dialog>'
 );		
 
 
@@ -30,20 +30,7 @@ toConfirmButton.addEventListener('click',function(){
 var confirmForm = document.getElementById("confirmForm");
 var submitButton = document.getElementById("submitButton");
 
-submitButton.addEventListener('click', function(){
-  const confirmFormData = new FormData(confirmForm);
-  const confirmAction = confirmForm.getAttribute("action");
-  const options = {
-    method: 'GET',
-    body: confirmFormData,
-  };
-  fetch(confirmAction, options).then((e) => {
-    if(e.status === 200) {
-      alert("保存しました。");
-      return;
-    }else{
-      alert("保存できませんでした。");
-      return;
-    }
-  });
+confirmForm.addEventListener('submit', function(){
+	confirmDialog.close();
+	return false;
 });
