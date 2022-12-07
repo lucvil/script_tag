35c1ca2de4d5e1bb9a4946561efdd9adc8580deb
lucvil <60900509+lucvil@users.noreply.github.com>
2022-12-07 19:05:52 +0900

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -1,7 +1,8 @@
 Shopify.Checkout.OrderStatus.addContentBox(
 	'<dialog id="inputDialog"><form id="inputForm"><input name="city" placeholder="市区町村" required="" type="text" aria-required="true" autocomplete="shipping address-level2"><input name="address1" placeholder="住所" required="" type="text" aria-required="true" autocomplete="shipping address-line1"><input name="address2" placeholder="建物名、部屋番号など (任意)" type="text" aria-required="false" autocomplete="shipping address-line2"><button id="toConfirmButton" type="button">確認</button></form></dialog>',
-	'<dialog id="confirmDialog"><form id="confirmForm"><button id="submitButton" type="button">送信</button></form></dialog>'
+	'<dialog id="confirmDialog" action="https://luckyvillages-sample.myshopify.com/apps/address/test" method="get"><form id="confirmForm"><button id="submitButton" type="button">送信</button></form></dialog>'
 );		
+
 
 var inputDialog = document.getElementById('inputDialog');
 
@@ -14,11 +15,14 @@ const toConfirmButton = document.getElementById("toConfirmButton");
 
 
 toConfirmButton.addEventListener('click',function(){
+  const inputFormData = new FormData(inputForm);
+  console.log(inputFormData);
   inputDialog.close();
   confirmDialog.showModal();
 };
 
-//データの受け渡しを実装
+
+
 
 const confirmForm = document.getElementById("confirmForm");
 const submitButton = document.getElementById("submitButton");
