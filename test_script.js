ce4811f00780e30ea311a9bcabdbb7d5a493921e
lucvil <60900509+lucvil@users.noreply.github.com>
2022-12-08 12:21:43 +0900

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -1,3 +1,33 @@
+// 日本語が入っているかを確認
+function includeJa(text) {
+try {
+    let gmi = 'gmi';
+    let regeIncludeHiragana = '^(?=.*[\u3041-\u3096]).*$';
+    let regeIncludeKatakana = '^(?=.*[\u30A1-\u30FA]).*$';
+    let regeIncludeKanji = '^(?=.*[\u4E00-\u9FFF]).*$';
+    let regeHiragana = new RegExp(regeIncludeHiragana, gmi);
+    let regeKatakana = new RegExp(regeIncludeKatakana, gmi);
+    let regeKanji = new RegExp(regeIncludeKanji, gmi);
+
+    let includeJa = false;
+    if (regeHiragana.test(text))
+	includeJa = true;
+    if (regeKatakana.test(text))
+	includeJa = true;
+    if (regeKanji.test(text))
+	includeJa = true;
+
+    return includeJa;
+} catch (error) {
+    alert(error);
+}
+}
+
+
+
+
+
+
 Shopify.Checkout.OrderStatus.addContentBox(
 	'<dialog id="inputDialog"><form id="inputForm"><input type="hidden" name="order_id" value=' + window.Shopify.checkout.order_id + '><input name="city" placeholder="市区町村" required="" type="text" aria-required="true" autocomplete="shipping address-level2"><input name="address1" placeholder="住所" required="" type="text" aria-required="true" autocomplete="shipping address-line1"><input name="address2" placeholder="建物名、部屋番号など (任意)" type="text" aria-required="false" autocomplete="shipping address-line2"><button id="toConfirmButton" type="button">確認</button></form></dialog>',
 	'<dialog id="confirmDialog"><form id="confirmForm" action="https://luckyvillages-sample.myshopify.com/apps/address/test" method="get"><button id="submitButton" type="button">送信</button></form></dialog>'
