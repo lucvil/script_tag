fc60b658aa892b92b6c9aa5558a3ed5218639833
lucvil <60900509+lucvil@users.noreply.github.com>
2022-12-08 12:22:06 +0900

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -1,26 +1,26 @@
 // 日本語が入っているかを確認
 function includeJa(text) {
-try {
-    let gmi = 'gmi';
-    let regeIncludeHiragana = '^(?=.*[\u3041-\u3096]).*$';
-    let regeIncludeKatakana = '^(?=.*[\u30A1-\u30FA]).*$';
-    let regeIncludeKanji = '^(?=.*[\u4E00-\u9FFF]).*$';
-    let regeHiragana = new RegExp(regeIncludeHiragana, gmi);
-    let regeKatakana = new RegExp(regeIncludeKatakana, gmi);
-    let regeKanji = new RegExp(regeIncludeKanji, gmi);
+	try {
+	    let gmi = 'gmi';
+	    let regeIncludeHiragana = '^(?=.*[\u3041-\u3096]).*$';
+	    let regeIncludeKatakana = '^(?=.*[\u30A1-\u30FA]).*$';
+	    let regeIncludeKanji = '^(?=.*[\u4E00-\u9FFF]).*$';
+	    let regeHiragana = new RegExp(regeIncludeHiragana, gmi);
+	    let regeKatakana = new RegExp(regeIncludeKatakana, gmi);
+	    let regeKanji = new RegExp(regeIncludeKanji, gmi);
 
-    let includeJa = false;
-    if (regeHiragana.test(text))
-	includeJa = true;
-    if (regeKatakana.test(text))
-	includeJa = true;
-    if (regeKanji.test(text))
-	includeJa = true;
+	    let includeJa = false;
+	    if (regeHiragana.test(text))
+		includeJa = true;
+	    if (regeKatakana.test(text))
+		includeJa = true;
+	    if (regeKanji.test(text))
+		includeJa = true;
 
-    return includeJa;
-} catch (error) {
-    alert(error);
-}
+	    return includeJa;
+	} catch (error) {
+	    alert(error);
+	}
 }
 
 
