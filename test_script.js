21cb2feb0fc8b6e0a2846c9959dd37dc81c9ff9c
lucvil <60900509+lucvil@users.noreply.github.com>
2022-12-09 13:50:29 +0900

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -12,15 +12,12 @@ function notIncludeJa(text) {
 	    let notIncludeJa = true;
 	    if (regeHiragana.test(text)){
 		notIncludeJa = false;
-		console.log("hiragana");
 	    }
 	    if (regeKatakana.test(text)){
 		notIncludeJa = false;
-		console.log("katakana");
 	    }
 	    if (regeKanji.test(text)){
 		notIncludeJa = false;
-		console.log("kanji");
 	    }
 
 	    return notIncludeJa;
@@ -36,33 +33,28 @@ function notIncludeJa(text) {
 if(window.Shopify.checkout.billing_address.country_code  == "JP" && notIncludeJa(window.Shopify.checkout.billing_address.city + window.Shopify.checkout.billing_address.address1)) {
 	Shopify.Checkout.OrderStatus.addContentBox(
 		'<dialog id="inputDialog"><form id="inputForm"><input type="hidden" name="order_id" value=' + window.Shopify.checkout.order_id + '><input name="city" placeholder="市区町村" required="" type="text" aria-required="true" autocomplete="shipping address-level2"><input name="address1" placeholder="住所" required="" type="text" aria-required="true" autocomplete="shipping address-line1"><input name="address2" placeholder="建物名、部屋番号など (任意)" type="text" aria-required="false" autocomplete="shipping address-line2"><button id="toConfirmButton" type="button">確認</button></form></dialog>',
-		'<dialog id="confirmDialog"><form id="confirmForm" action="https://luckyvillages-sample.myshopify.com/apps/address/test" method="get"><button id="submitButton" type="button">送信</button></form></dialog>'
 	);		
 
-
 	var inputDialog = document.getElementById('inputDialog');
-
-	inputDialog.showModal();
-
-
-
-	var confirmDialog = document.getElementById("confirmDialog");
 	var inputForm = document.getElementById("inputForm");
 	var toConfirmButton = document.getElementById("toConfirmButton");
+	inputDialog.showModal();
 
-
+	var confirmDialog
 	var inputFormData;
+	
 	toConfirmButton.addEventListener('click',function(){
 		inputFormData = new FormData(inputForm);
 		console.log(inputFormData.get('city'));
 		inputDialog.close();
+		Shopify.Checkout.OrderStatus.addContentBox(
+			'<dialog id="confirmDialog"><p>'+inputFormData.get('city')+'</p><button id="submitButton" type="button">送信</button></dialog>'
+		);
+		confirmDialog =  = document.getElementById("confirmDialog");
 		confirmDialog.showModal();
 	});
 
 
-
-
-	var confirmForm = document.getElementById("confirmForm");
 	var submitButton = document.getElementById("submitButton");
 
 	submitButton.addEventListener('click', function(){
